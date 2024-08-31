bb2cccf8b60759dd68d52d962a94de938015a7cf
MaiconJordan <maiconplus@gmail.com>
2024-08-31 20:30:10 -0300

    lambda

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,13 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -20,5 +24,26 @@ public class Principal {
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i <= dados.totalTemporadas(); i ++){
+        	json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+        	DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+        	temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+
+       // interação de coleção de forma antiga
+       //  for(int i = 0; i < dados.totalTemporadas(); i++){
+       //    List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+       //  for(int j = 0; j< episodiosTemporada.size(); j++){
+        //    System.out.println(episodiosTemporada.get(j).titulo());
+        //     }
+        //}
+
+        // integração de coleção de forma melhorada
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
